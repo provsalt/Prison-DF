7083998862f7254c32639cfe4e1d3a5ab4ed426a
provsalt <gooraymond39@gmail.com>
2020-11-02 00:35:52 +0800

    Fix compiler directives. I need sleep

--- prisons/utils/utils.go
+++ prisons/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/df-mc/dragonfly/dragonfly/player"
 	"github.com/df-mc/dragonfly/dragonfly/session"
 	"github.com/sandertv/gophertunnel/minecraft"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 	"github.com/sandertv/gophertunnel/minecraft/text"
 	"github.com/sirupsen/logrus"
 	_ "unsafe"
@@ -26,14 +27,14 @@ func GetLogger() *logrus.Logger {
 	return Logger
 }
 
-//go:linkname player_session github.com/df-mc/dragonfly/dragonfly/player.(*Player).session
+//go:linkname Player_session github.com/df-mc/dragonfly/dragonfly/player.(*Player).session
 //noinspection ALL
 func Player_session(*player.Player) *session.Session
 
-//go:linkname session_connection github.com/df-mc/dragonfly/dragonfly/session.(*Session).connection
+//go:linkname Session_connection github.com/df-mc/dragonfly/dragonfly/session.(*Session).connection
 //noinspection ALL
 func Session_connection(*session.Session) *minecraft.Conn
 
-//go:linkname session_writePacket github.com/df-mc/dragonfly/dragonfly/session.(*Session).writePacket
+//go:linkname Session_writePacket github.com/df-mc/dragonfly/dragonfly/session.(*Session).writePacket
 //noinspection ALL
 func Session_writePacket(*session.Session, packet.Packet)
