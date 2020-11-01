9e8eab57705935b14c62eee6781515b389f93ed9
provsalt <gooraymond39@gmail.com>
2020-11-02 00:27:09 +0800

    Colorize is now useless here. Use text.Colourf is now

--- prisons/utils/utils.go
+++ prisons/utils/utils.go
@@ -7,7 +7,6 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft"
 	"github.com/sandertv/gophertunnel/minecraft/text"
 	"github.com/sirupsen/logrus"
-	"regexp"
 	_ "unsafe"
 )
 
@@ -19,10 +18,6 @@ func GetPrefix() string {
 	return text.Colourf("<b><green>Salt</green><yellow>Craft</yellow></b>")
 }
 
-func Colorize(message string) string {
-	r := regexp.MustCompile("/&([0-9a-fk-or])/u")
-	return r.ReplaceAllString(message, "§$1"+message)
-}
 func GetServer() *dragonfly.Server {
 	return Server
 }
