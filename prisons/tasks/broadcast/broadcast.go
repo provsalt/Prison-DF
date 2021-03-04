7ca35bf5eecf5484bba94777f069707af52d9b72
Raymond <39023494+provsalt@users.noreply.github.com>
2021-03-04 23:25:21 +0800

    Merge remote-tracking branch 'origin/master'

--- prisons/tasks/broadcast/broadcast.go
+++ prisons/tasks/broadcast/broadcast.go
@@ -2,19 +2,21 @@ package broadcast
 
 import (
 	"Prison/prisons/utils"
+
 	"github.com/df-mc/dragonfly/dragonfly/player/chat"
 
-	"github.com/sandertv/gophertunnel/minecraft/text"
 	"time"
+
+	"github.com/sandertv/gophertunnel/minecraft/text"
 )
 
 var Messages = [...]string{
-	text.Colourf(utils.Broadcastprefix + "Join our discord today at discord.saltcraft.xyz\n"),
-	text.Colourf(utils.Broadcastprefix + "Remember to vote for our server at vote.saltcraft.xyz for rewards\n"),
-	text.Colourf(utils.Broadcastprefix + "Follow our twitter @saltcraft\n"),
+	text.Colourf(utils.Broadcastprefix + "Join our discord today at discord.,moonlightpe.com\n"),
+	text.Colourf(utils.Broadcastprefix + "Remember to vote for our server at vote.moonlightpe.com for rewards\n"),
+	text.Colourf(utils.Broadcastprefix + "Follow our twitter @RealMoonlightPE\n"),
 	text.Colourf(utils.Broadcastprefix + "Check dragonfly out, our server software at github.com/df-mc/dragonfly\n"),
 	text.Colourf(utils.Broadcastprefix + "Subscribe to provsalt on <red>youtube</red>\n"),
-	text.Colourf(utils.Broadcastprefix + "Check out our store at store.saltcraft.xyz\n"),
+	text.Colourf(utils.Broadcastprefix + "Check out our store at store.moonlightpe.com\n"),
 	text.Colourf(utils.Broadcastprefix + "Rebirths are an essential part of the game /rebirth for more info!"),
 	text.Colourf(utils.Broadcastprefix + "/help is a great place to start"),
 }
