f9a1da641d2f23e70485350ce5fd25e8e3cb2bee
Raymond <39023494+provsalt@users.noreply.github.com>
2021-03-05 20:43:36 +0800

    Revert minereset changes

--- prisons/tasks/minereset/reset.go
+++ prisons/tasks/minereset/reset.go
@@ -3,7 +3,8 @@ package minereset
 import (
 	"Prison/prisons/mines"
 	"github.com/df-mc/dragonfly/dragonfly/world"
-	"sync"
+	"math/rand"
+	"time"
 )
 
 type MineReset struct {
@@ -15,22 +16,17 @@ func (m MineReset) Dimensions() [3]int {
 }
 
 func (m MineReset) At(x, y, z int, blockAt func(x int, y int, z int) world.Block) world.Block {
-	// TODO BIG TODO MAKE USE OF CHANNELS AND GOROUTINES this is way too slow
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	// TODO I can probably use len() and just rand for this instead
-
-	// for block, i := range m.Mine.Blocks {
-	// 	chance := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(100)
-	// 	for k, j := range i.([2]int) {
-	// 		// k refers to the lowest
-	// 		// j refers to the highest
-	// 		if chance >= k && chance <= j {
-	// 			return block
-	// 		}
-	// 		continue
-	// 	}
-	// }
+	for block, i := range m.Mine.Blocks {
+		chance := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(100)
+		for k, j := range i.([2]int) {
+			// k refers to the lowest
+			// j refers to the highest
+			if chance >= k && chance <= j {
+				return block
+			}
+			continue
+		}
+	}
 	return nil
 }
 
